f0c163756806b03bf85764c5b957a333f380b6a2
Stefal <stephane.peneau@wanadoo.fr>
2023-01-30 20:50:53 +0100

    Update config.py
    Add informations in the sourcetable.
    With this commit, app like Lefebure Ntrip Client will display the mount name list.

--- pybasevar/config.py
+++ pybasevar/config.py
@@ -4,7 +4,8 @@ from decimal import *
 ## 00-START socat TODO
 socat="socat -d -d pty,raw,echo=0 pty,raw,echo=0 &>/dev/null"
 ## 01-Start caster
-ntripc="str2str -in serial://pts/2:115200:8:n:1:off -n 1 -b 1 -out ntripc://@:9999/ME  &>/dev/null"
+# "mount point name or identifier; signal format (RTCM); signal details (list of rtcm messages); carrier phase information ; nav system (GPS+GLO+ ...);network name;country;latitude;longitude;require nmea;solution type (base, network); stream generator; compression algorithm; authentification; fee; bitrate,miscellaneous info
+ntripc="str2str -in serial://pts/2:115200:8:n:1:off -n 1 -b 1 -out ntripc://@:9999/ME:ME;RTCM3;;2;GPS+GLO+GAL+BDS+QZS;NONE;NONE;;;0;0;RTKBaseVar;NONE;N;N;100;  &>/dev/null"
 ## 02-Start a generic stream RTCM3 in
 stream1="str2str -in ntrip://:@caster.centipede.fr:80/"
 stream2=" -out serial://pts/1:115200:8:n:1:off &>/dev/null"
